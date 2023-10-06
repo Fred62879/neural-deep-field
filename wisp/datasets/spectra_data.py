83d5f04ccc8c12c53638fb721815a5c86a700b60
Fred62879 <zhuotingxie-fred@outlook.com>
2023-10-05 20:54:42 -0700

    100 to 100 testing

--- wisp/datasets/spectra_data.py
+++ wisp/datasets/spectra_data.py
@@ -490,11 +490,11 @@ class SpectraData:
             np.random.seed(20)
             np.random.shuffle(c)
             self.redshift_pretrain_ids = c[:self.kwargs["redshift_pretrain_num_spectra"]]
-            self.redshift_pretrain_ids = np.array([
-                26,86,2,55,75, 93,16,73,54,95, 53,92,78,13,7, 30,22,24,33,8
-            ])
+            # self.redshift_pretrain_ids = np.array([
+            #     26,86,2,55,75, 93,16,73,54,95, 53,92,78,13,7, 30,22,24,33,8
+            # ])
             validation_ids = supervision_ids[self.redshift_pretrain_ids]
-            print(self.redshift_pretrain_ids)
+            # print(self.redshift_pretrain_ids)
 
         # log.info(f"test spectra ids: {test_ids}")
         # log.info(f"validation spectra ids: {validation_ids}")
