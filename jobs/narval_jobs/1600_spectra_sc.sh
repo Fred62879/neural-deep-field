0a174b53d60769af51e29c7a1359aef65024d8d2
fred62879 <zhuotingxie-fred@outlook.com>
2024-02-23 00:46:31 -0500

    exp script updated on narval

--- jobs/narval_jobs/1600_spectra_sc.sh
+++ jobs/narval_jobs/1600_spectra_sc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --account=def-kyi-ab
-#SBATCH --job-name=1600_spectra_sc_regu_beta
+#SBATCH --job-name=1600_spectra_sc_all
 #SBATCH --output=./outputs/%x-%j.out
 #SBATCH --ntasks=3
 #SBATCH --mem-per-cpu=40000
@@ -11,12 +11,13 @@
 source ~/envs/wisp_env/bin/activate
 cd ../../
 
-#python app/main_astro.py --config configs/1600-spectra-3-layers-skip.yaml
-#python app/main_astro.py --config configs/1600-spectra-3-layers-no-skip.yaml
-#python app/main_astro.py --config configs/1600-spectra-3-layers-skip-same-dim.yaml
-#python app/main_astro.py --config configs/1600-spectra-regu-beta-16-3-layers-skip-same-dim-sep-layers.yaml
+python app/main_astro.py --config configs/1600-spectra-skip-sc.yaml
+python app/main_astro.py --config configs/1600-spectra-no-skip-sc.yaml
+python app/main_astro.py --config configs/1600-spectra-skip-same-dim-sc.yaml
+python app/main_astro.py --config configs/1600-spectra-skip-same-dim-sep-layers-sc.yaml
 
-python app/main_astro.py --config configs/1600-spectra-regu-beta-1-3-layers-skip-same-dim-sep-layers.yaml
-python app/main_astro.py --config configs/1600-spectra-regu-beta-2-3-layers-skip-same-dim-sep-layers.yaml
-python app/main_astro.py --config configs/1600-spectra-regu-beta-4-3-layers-skip-same-dim-sep-layers.yaml
-python app/main_astro.py --config configs/1600-spectra-regu-beta-8-3-layers-skip-same-dim-sep-layers.yaml
+# python app/main_astro.py --config configs/1600-spectra-regu-beta-1-3-layers-skip-same-dim-sep-layers.yaml
+# python app/main_astro.py --config configs/1600-spectra-regu-beta-2-3-layers-skip-same-dim-sep-layers.yaml
+# python app/main_astro.py --config configs/1600-spectra-regu-beta-4-3-layers-skip-same-dim-sep-layers.yaml
+# python app/main_astro.py --config configs/1600-spectra-regu-beta-8-3-layers-skip-same-dim-sep-layers.yaml
+#python app/main_astro.py --config configs/1600-spectra-regu-beta-16-3-layers-skip-same-dim-sep-layers.yaml
--- jobs/narval_jobs/1600_spectra_skip_same_dim_sep_layers.sh
+++ jobs/narval_jobs/1600_spectra_skip_same_dim_sep_layers.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#SBATCH --array=1
 #SBATCH --time=4:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --account=def-kyi-ab
-#SBATCH --job-name=1600_spectra_regu_beta_16_skip_same_dim_sep_layers
+#SBATCH --job-name=1600_spectra_skip_same_dim_sep_layers_bn_false
 #SBATCH --output=./outputs/%x-%j.out
 #SBATCH --ntasks=3
 #SBATCH --mem-per-cpu=40000
@@ -11,4 +12,4 @@
 source ~/envs/wisp_env/bin/activate
 cd ../../
 
-python app/main_astro.py --config configs/1600-spectra-regu-beta-16-3-layers-skip-same-dim-sep-layers.yaml
+python app/main_astro.py --config configs/1600-spectra-skip-same-dim-sep-layers.yaml --ablat-id $SLURM_ARRAY_TASK_ID
