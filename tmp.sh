388cb46e39f84b64822b67a3cbb10e3c239588ba
Fred62879 <zhuotingxie-fred@outlook.com>
2024-02-26 17:51:10 -0800

    debugged decoder, updated config files

---
+++ tmp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 0
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 1
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 2
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 3
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 4
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 5
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 6
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 7
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 8
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 9
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 10
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 11
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 12
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 13
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 14
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 15
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 16
+python app/main_astro.py --config configs/3200-spectra-skip-add-convert-input-sc.yaml --ablat-id 17
