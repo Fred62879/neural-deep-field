4c3b0617eb83b02028f8f8a59bb5a6863c9b41f0
fred62879 <zhuotingxie-fred@outlook.com>
2024-05-19 13:24:33 -0400

    exp update on narval

--- jobs/narval_jobs/pretrain0.sh
+++ jobs/narval_jobs/pretrain0.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --time=2:00:00
+#SBATCH --time=14:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --account=def-kyi-ab
-#SBATCH --job-name=dz-spectra-3-512-regu-no-skip-regu-beta-32
+#SBATCH --job-name=vipers-spectra-pretrain
 #SBATCH --output=./outputs/%x-%j.out
 #SBATCH --ntasks=3
-#SBATCH --mem-per-cpu=20000
+#SBATCH --mem-per-cpu=40000
 
 source ~/envs/wisp_env/bin/activate
 cd ../../
 
-python app/main_astro.py --config configs/narval_configs/pretrain_tmp.yaml --ablat-id 0
+#python app/main_astro.py --config configs/narval_configs/pretrain.yaml
+python app/main_astro.py --config configs/narval_configs/pretrain_tmp.yaml
